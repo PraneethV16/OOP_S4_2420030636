acb7638a8b647ed643dc145d88e0b16f023ac1e7
PraneethV16 <2420030636@klh.edu.in>
2025-08-12 11:54:03 +0530

    Add files via upload

---
+++ Strings/ReadAndPrint.java
@@ -0,0 +1,13 @@
+package Strings;
+import java.util.*;
+public class ReadAndPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter a the string");
+          String s=sc.nextLine();
+          System.out.println("Display the String: "+s);
+	}
+
+}
---
+++ Strings/StringObject.java
@@ -0,0 +1,16 @@
+package Strings;
+import java.util.Scanner;
+public class StringObject {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the String obj Value:");
+    String s = new String(sc.nextLine());
+    System.out.println("Entered String: "+ s);
+    System.out.println("string.lenght() = "+s.length());
+    sc.close();
+
+  }
+
+}
